98f65a1bb13d536df4a7d039a79ec84a201eded1
Simarpreet Kaur <shbsachdeva64@gmail.com>
2021-06-28 01:25:17 +0530

    app running

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const developers = require('./developers/Developers');
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Server home!');
-});
-
 app.use('/developers', developers);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('devrepoclient/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join('devrepoclient','build', 'index.html'),{ root: __dirname });
+  });
+}
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`)
 });
